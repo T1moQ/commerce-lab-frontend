052c51e218b6181087aaf7eadc1b560ca5749242
TimoQ <pttima@yandex.ru>
2026-03-09 15:53:23 +0300

    feat: pagination UI added

--- src/pages/products/ui/products-page.tsx
+++ src/pages/products/ui/products-page.tsx
@@ -13,13 +13,52 @@ export const ProductsPage: FC = () => {
 
   const { data, isLoading, error } = useProducts({ page, perPage: PER_PAGE })
 
-  if (isLoading) return <div>Loading...</div>
+  const products = data?.items ?? []
+  const total = data?.total ?? 0
+  const totalPages = Math.ceil(total / PER_PAGE)
+
+  const handlePrev = () => {
+    if (page <= 1) return
+
+    setSearchParams({
+      page: String(page - 1)
+    })
+  }
+
+  const handleNext = () => {
+    if (page >= totalPages) return
+
+    setSearchParams({
+      page: String(page + 1)
+    })
+  }
+
+  if (!data && isLoading) return <div>Loading...</div>
   if (error) return <div>Error</div>
 
   return (
     <main className="flex flex-col justify-center items-center p-8">
-      Playground page
-      <ProductsGrid products={data?.items ?? []} />
+      <h1 className="mb-6 text-2xl font-bold">Products page</h1>
+      <ProductsGrid products={products} />
+      <div className="flex items-center mt-8 gap-2">
+        <button
+          onClick={handlePrev}
+          disabled={page === 1}
+          className="rotate-180 cursor-pointer"
+        >
+          →
+        </button>
+        <span>
+          Page {page} of {totalPages || 1}
+        </span>
+        <button
+          onClick={handleNext}
+          disabled={page >= totalPages}
+          className="cursor-pointer"
+        >
+          →
+        </button>
+      </div>
     </main>
   )
 }
