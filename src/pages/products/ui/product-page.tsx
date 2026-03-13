6bb88c105dcd7419001b260f545b94575c805026
TimoQ <pttima@yandex.ru>
2026-03-13 15:52:00 +0300

    fix: routing

--- src/pages/products/ui/product-page.tsx
+++ src/pages/products/ui/product-page.tsx
@@ -1,11 +1,10 @@
 import { Button } from '@/components/ui/button'
 import { useProductBySlug } from '@/entities/product/api/queries'
 import type { FC } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
 export const ProductPage: FC = () => {
   const slug = useParams().slug
-  const navigate = useNavigate()
   const { data, isLoading, error } = useProductBySlug(slug ?? '')
 
   if (isLoading) return <div>Loading...</div>
@@ -14,9 +13,9 @@ export const ProductPage: FC = () => {
 
   return (
     <main className="flex flex-col justify-center items-center relative">
-      <Button className="absolute top-2 left-2" onClick={() => navigate(-1)}>
-        Back to list
-      </Button>
+      <Link to={'/products'}>
+        <Button className="absolute top-2 left-2">Back to list</Button>
+      </Link>
       <h2>Product Detail Page</h2>
       <h3>{data.title}</h3>
       <p>{data.desc}</p>
